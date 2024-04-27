9cb87601ed23902a5d50642916794fed59ba50da
Aleksandr Chashchin <achasch@gmail.com>
2024-04-27 22:50:13 +0200

    add basic vue flow

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
+import { VueFlow, useVueFlow } from '@vue-flow/core';
+import { ref } from 'vue';
+
+const nodes = ref([
+  { id: '1', type: 'input', label: 'Node 1', position: { x: 0, y: 0 }},
+  { id: '2', type: 'input', label: 'Node 2', position: { x: 50, y: 50 }},
+]);
+
+const edges = ref([
+  { id: 'e1-2', source: '1', target: '2' },
+]);
+
+const { onPaneReady, fitView } = useVueFlow();
+
+onPaneReady(() => fitView());
 </script>
 
 <template>
-  <div>Hi</div>
+  <div class="graph">
+    <VueFlow
+      :nodes="nodes"
+      :edges="edges"
+      :default-viewport="{ zoom: 1.5 }"
+      :min-zoom="0.2"
+      :max-zoom="4"
+    >
+    </VueFlow>
+  </div>
 </template>
 
 <style scoped>
+.graph {
+  width: 600px;
+  height: 600px;
+}
 </style>
